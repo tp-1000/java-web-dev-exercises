6aba691f114240bad880def10b93354ccdd99d5f
thomaspetty <thomaskpetty@gmail.com>
2020-04-09 10:07:50 -0500

    Added custom name input. Used import java.util.Scanner, created a scanner instance. Collected Input from the command line and passed it to the message class method getMessage.

--- src/org/launchcode/java/demos/lsn1datatypes/HelloMethods.java
+++ src/org/launchcode/java/demos/lsn1datatypes/HelloMethods.java
@@ -1,9 +1,17 @@
 package org.launchcode.java.demos.lsn1datatypes;
 
+import javax.print.DocFlavor;
+import java.util.Scanner;
+
 public class HelloMethods {
 
     public static void main(String[] args) {
-        String message = Message.getMessage("fr");
+        Scanner input;
+
+        input = new Scanner(System.in);
+        System.out.println("whats your name?");
+        String aName = input.next();
+        String message = Message.getMessage("Eng", aName);
         System.out.println(message);
     }
 
--- src/org/launchcode/java/demos/lsn1datatypes/Message.java
+++ src/org/launchcode/java/demos/lsn1datatypes/Message.java
@@ -2,14 +2,14 @@ package org.launchcode.java.demos.lsn1datatypes;
 
 public class Message {
 
-    public static String getMessage(String lang) {
+    public static String getMessage(String lang, String name) {
 
         if (lang.equals("sp")) {
             return "¡Hola, Mundo!";
         } else if (lang.equals("fr")) {
             return "Bonjour, le monde!";
         } else {
-            return "Hello, World!";
+            return "Hello ".concat(name).concat(" and World!");
         }
     }
 }
