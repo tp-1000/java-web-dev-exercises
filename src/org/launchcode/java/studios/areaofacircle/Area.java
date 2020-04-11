8754ebefbffd0909bf90248e9b7d5a5e2a24ca4d
thomaspetty <thomaskpetty@gmail.com>
2020-04-10 19:23:36 -0500

    added validation to Area Class

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,5 +1,6 @@
 package org.launchcode.java.studios.areaofacircle;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import static java.lang.Math.round;
 
@@ -7,15 +8,34 @@ import static java.lang.Math.round;
 public class Area {
 
     public static void main (String[] args) {
-        //ask for radius
+        //ask for radius -- validate with message -- repeat until valid input is provided
+        //request radius
         System.out.println("What is your radius: ");
         Scanner input = new Scanner(System.in);
-        double r = input.nextDouble();
-        input.close();
+        double r;
 
-        // print area calculation
+        //start loop and continue until valid input is provided.
+        while(true) {
+            try {
+                r = input.nextDouble();
+                if(r <= 0) {
+                    System.out.println("Invalid entry,\nplease enter a number above 0: ");
+                    input = new Scanner(System.in);
+                } else {
+                    input.close();
+                    break;
+                }
+            }
+            catch (InputMismatchException nfe) {
+                System.out.println("Invalid entry,\nplease enter a number above 0: ");
+                input = new Scanner(System.in);
+            }
+        }
+
+
+
+        // print getArea() result.
         double area = Circle.getArea(r);
         System.out.println("The area for circle of radius " + r + " is: " + round(area*1000)/1000.);
     }
 }
-
