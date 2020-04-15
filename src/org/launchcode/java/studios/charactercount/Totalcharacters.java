33c22781f2ac7404080b7a1c20b8f387eff375bf
thomaspetty <thomaskpetty@gmail.com>
2020-04-14 19:59:16 -0500

    Totalcharacters.java works as intended

--- src/org/launchcode/java/studios/charactercount/Totalcharacters.java
+++ src/org/launchcode/java/studios/charactercount/Totalcharacters.java
@@ -1,16 +1,50 @@
 package org.launchcode.java.studios.charactercount;
 
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Totalcharacters {
 
     public static void main(String[] args) {
+        //ToDo Bonus input file
+        //ToDo input string
 
-        //pass string and char (obtained through grex or maybe a unicode iterationloop, return int for passed char
-        countChar('a', "Diablo");
+        //import string
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter text for character counts: ");
+        String[] text = input.nextLine().toLowerCase().split("");
+
+
+        for (Map.Entry<String, Integer> entry: buildCountList((text)).entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+
     }
 
-    private static int countChar(char letter, String text) {
-        System.out.println(letter + text);
-        return 1;
+    private static HashMap<String, Integer> buildCountList(String[] text) {
+        HashMap<String, Integer> counts = new HashMap<>();
+
+        //build hashmap from first letter in text[0] -- only alph chars
+        for (String letter : text) {
+            if (letter.matches("[a-z]")) {
+                int k = countChar(letter, text);
+                counts.put(letter, k);
+            }
+        }
+        return counts;
+    }
+
+    private static int countChar(String letter, String[] text) {
+        //count times letter is in text -- return the count
+        int charCount = 0;
+        for (String aLetter: text) {
+            if (aLetter.toLowerCase().equals(letter)) {
+                charCount++;
+            }
+        }
+        return charCount;
     }
 }
